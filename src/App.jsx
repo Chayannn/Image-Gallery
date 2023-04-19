e932479d2142607b8f2f68c2f023e7a1e06e936f
Chayannn <chayanp188@gmail.com>
2023-04-20 00:19:34 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
+function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=35570979-d38a0e590c88d9af6c773d092&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image) => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
